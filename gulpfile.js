44211f03a26a031558c4c32b63486840cf7ba813
Art San Diego <asd.artsandiego@gmail.com>
2017-09-11 16:02:01 +0800

    Initial Commit

---
+++ gulpfile.js
@@ -0,0 +1,43 @@
+'use strict';
+
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var ggcmq  = require('gulp-group-css-media-queries');
+var autoprefixer = require('gulp-autoprefixer');
+
+var sassPath = {
+    src : 'res/scss/**/*.scss',
+    dist : 'dist/'
+}
+
+var cssPath = 'dist/app.css';
+ 
+gulp.task('sass', function () {
+  return gulp.src(sassPath.src)
+    .pipe(sass({
+        outputStyle: 'expanded', 
+        errLogToConsole: true
+    }).on('error', sass.logError))
+    .pipe(autoprefixer({
+        browsers: [
+            'last 2 versions',
+            'ie >= 10',
+            'ie_mob >= 10',
+            'ff >= 30',
+            'chrome >= 34',
+            'safari >= 7',
+            'opera >= 23',
+            'ios >= 7',
+            'android >= 4.4',
+            'bb >= 10'
+        ]
+    }))
+    .pipe(ggcmq({
+        log: true
+    }))
+    .pipe(gulp.dest(sassPath.dist));
+});
+ 
+gulp.task('default', function () {
+  gulp.watch(sassPath.src, ['sass']);
+});
